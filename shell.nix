9056d05e77a1822c2a1fccb07debc250eb232894
David <davidm@newside.org>
2026-01-26 16:43:20 -0600

    for Rust development

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    pkg-config
+    gtk3
+    gtk4
+    glib
+    cairo
+    gdk-pixbuf
+    librsvg
+    webkitgtk_4_1
+    webkitgtk_6_0
+    cargo
+    cargo-tauri
+    rustc
+    rustfmt
+    openssl
+  ];
+}
